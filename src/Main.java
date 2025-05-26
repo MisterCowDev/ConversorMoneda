51b8d10e4a08d1d27691385cd102714d548580fe
mistercow <esteban.lagos21@gmail.com>
2025-05-25 20:44:20 -0400

    Agregado el try catch para capturar error en casos de ingresar mal los datos

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -6,21 +7,28 @@ public class Main {
         CurrecyConverter currecyConverter = new CurrecyConverter();
 
         while (true){
-            System.out.println("\n****** Bienvenido al corvertidor de monedas ******\n");
-            System.out.println("1) Dolar ==>> Peso Chileno");
-            System.out.println("2) Peso Chileno ==>> Dolar");
-            System.out.println("3) Dolar ==>> Real Brasileño");
-            System.out.println("4) Real Brasileño ==>> Dolar");
-            System.out.println("5) Dolar ==>> Peso Argentino");
-            System.out.println("6) Peso Argentino ==>> Dolar");
-            System.out.println("7) Salir");
-            System.out.print("Selecciona una de las siguientes opciones: ");
-            int option = scanner.nextInt();
-            if (option >= 1 && option <= 6) {
-                currecyConverter.menuOption(option);
-            } else if (option == 7) {
-                System.out.println("\nGracias por usar el convertidor de monedas");
-                break;
+            try {
+                System.out.println("\n****** Bienvenido al corvertidor de monedas ******\n");
+                System.out.println("1) Dolar ==>> Peso Chileno");
+                System.out.println("2) Peso Chileno ==>> Dolar");
+                System.out.println("3) Dolar ==>> Real Brasileño");
+                System.out.println("4) Real Brasileño ==>> Dolar");
+                System.out.println("5) Dolar ==>> Peso Argentino");
+                System.out.println("6) Peso Argentino ==>> Dolar");
+                System.out.println("7) Salir");
+                System.out.print("Selecciona una de las siguientes opciones con un número: ");
+                int option = scanner.nextInt();
+                if (option >= 1 && option <= 6) {
+                    currecyConverter.menuOption(option);
+                } else if (option == 7) {
+                    System.out.println("\nGracias por usar el convertidor de monedas");
+                    break;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("\nDebe ingresar una opción valida");
+                scanner.nextLine();
+            } catch (Exception  e) {
+                throw new RuntimeException(e);
             }
         }
 
